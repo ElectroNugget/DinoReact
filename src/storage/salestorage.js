8eae91e72db082dfd593ec8fa1ffec89b0589cf2
John Henrik Muller <johnhenrikmuller@gmail.com>
2021-05-31 14:50:18 +0200

    Made a storage for the sales cards.

---
+++ src/storage/salestorage.js
@@ -0,0 +1,27 @@
+export const salesCards = [
+  {
+    title: "Discounted Dinos",
+    description: "Check out our collection of small dinosaurs!",
+    imgUrl: "/images/specials/DiscountedDinos.png",
+    iconUrl: "fas fa-tags",
+  },
+  {
+    title: "Best Sellers",
+    description: "Stegosaurus never ceases to be a family favourite",
+    imgUrl: "/images/specials/BestSeller.png",
+    iconUrl: "fas fa-chart-line",
+  },
+  {
+    title: "Featured Dinosaur",
+    description:
+      "InGen has just made their new Brachiosaurus model widely available.",
+    imgUrl: "/images/specials/FeaturedDinosaur.png",
+    iconUrl: "fas fa-medal",
+  },
+  {
+    title: "Lightning Deal",
+    description: "Pteranodons are 50% off for the next 24hrs! Don't miss it!",
+    imgUrl: "/images/specials/LightningDeal.png",
+    iconUrl: "fas fa-dollar-sign",
+  },
+];
