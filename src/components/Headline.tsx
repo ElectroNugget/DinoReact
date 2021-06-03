603407299c66770c227eb128238fe6fb51208670
John Henrik Muller <johnhenrikmuller@gmail.com>
2021-06-04 00:22:58 +0200

    Fixed headline!!!!

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -1,35 +1,13 @@
 import { useContext } from "react";
 import "../css/stylesheet.css";
 import { UserContext } from "../UserContext";
-//TODO: How to handle customerID?
+
 type HeadlineProps = {
   message: string;
 };
 
 function Headline({ message }: HeadlineProps): JSX.Element {
-  //TODO: Need to do something here... Get UID and fName from API when registering.
-  let customerId: number = 1;
-
-  //Standard way of doing it.
-  //I AM SUBSCRIBING TO USERCONTEXT SO I CAN USE THESE FIELDS IN THIS COMPONENT
-  const { firstName, setFirstName } = useContext(UserContext);
-
-  async function getUserName() {
-    await fetch(`http://localhost:8000/customers/${customerId}`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json;charset=utf-8" },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setFirstName(data.firstName);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  getUserName();
-
+  const { user } = useContext(UserContext);
   return (
     <div>
       <UserContext.Consumer>
@@ -37,7 +15,7 @@ function Headline({ message }: HeadlineProps): JSX.Element {
           if (loggedIn) {
             return (
               <h3>
-                Hello, {firstName}
+                Hello, {user.firstName}
                 {message}
               </h3>
             );
