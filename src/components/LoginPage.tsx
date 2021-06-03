8d8ad0d069b9751a3f472ec96baa570d7446be18
John Henrik Muller <johnhenrikmuller@gmail.com>
2021-06-04 01:19:06 +0200

    PUUSH (Logging in works now)

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -6,24 +6,57 @@ import Headline from "./Headline";
 import "../css/stylesheet.css";
 
 import { UserContext } from "../UserContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 function LoginPage(): JSX.Element {
   //Standard way of doing it.
   //I AM SUBSCRIBING TO USERCONTEXT SO I CAN USE THESE FIELDS IN THIS COMPONENT
-  const { loggedIn, setLoggedIn } = useContext(UserContext); 
+  const { loggedIn, setLoggedIn, user, setUser } = useContext(UserContext);
+  const [email, setEmail] = useState();
+
   console.log("Are you logged in?", loggedIn);
 
   let message: string;
 
   if (loggedIn) {
-    message = ", you are already logged in.";
+    message = ", click to log out.";
   } else {
     message = ", please login by providing your email here.";
   }
 
+  async function login() {
+    console.log("We're sending this email", email);
+    console.log("This is it stringified", JSON.stringify(email));
+    await fetch(`http://localhost:8000/customers/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json;charset=utf-8" },
+      body: JSON.stringify({ email: email }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        setLoggedIn(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Incorrect login details provided!");
+      });
+  }
+
+  function logout() {
+    setLoggedIn(false);
+    setUser({});
+  }
+
+  function updateEmail(event: any) {
+    setEmail(event.target.value);
+  }
+
   return (
-    <div className="container" style={{ width: "60%", paddingBottom: "2.5rem" }}>
+    <div
+      className="container"
+      style={{ width: "60%", paddingBottom: "2.5rem" }}
+    >
       <div className="card">
         <div className="card-header">Login</div>
         <main className="form-signin">
@@ -32,25 +65,30 @@ function LoginPage(): JSX.Element {
               <Headline message={message} />
               <br />
               {loggedIn ? (
-                <button onClick={() => setLoggedIn(false)}>Log Out</button>
+                <button onClick={() => logout()}>Log Out</button>
               ) : (
                 <div>
                   <div className="form-group">
                     <input
-                      id="email"
-                      type="email"
+                      type="text"
+                      placeholder="Enter your name"
                       className="form-control"
-                      placeholder="Email"
+                      onChange={(event) => updateEmail(event)}
                       required
                       autoFocus
                     />
                   </div>
                   <br />
                   <div>
-                    <button onClick={() => setLoggedIn(true)}>Log In</button>
+                    <button onClick={() => login()}>Log In</button>
                   </div>
                 </div>
               )}
+              <button
+                onClick={() => console.log("This is the state of user:", user)}
+              >
+                Check User
+              </button>
             </div>
           </div>
         </main>
