dd849cd4ca8c6144151c60a2265d82e2d379113f
John Henrik Muller <jhmu@itu.dk>
2021-06-08 15:40:07 +0200

    Added a code snippet for a new input.

--- src/components/RegisterPage.tsx
+++ src/components/RegisterPage.tsx
@@ -155,4 +155,14 @@ function RegisterPage(): JSX.Element {
   );
 }
 
+// Pretty cool thingy I need to add as new input.
+{/* <label className="header-name">
+<input
+  value={name}
+  onChange={e => setName(e.target.value)}
+  onClick={e => e.target.setSelectionRange(0, e.target.value.length)}
+  placeholder="Untitled"
+/>
+</label> */}
+
 export default RegisterPage;
